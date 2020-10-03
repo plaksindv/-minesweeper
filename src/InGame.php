f35981830d5eb7f5ab8e36adc9f76e6af61ccb5b
plaksindv <dmitriy.plaksin1@yandex.ru>
2020-10-03 10:49:27 +0300

    Test commit

---
+++ src/InGame.php
@@ -0,0 +1,59 @@
+<?php namespace plaksindv\minesweeper\InGame;
+function openSurroundedCells($x, $y)
+{
+    global $cellsArray;
+    if (isset($cellsArray[$y]) 
+        && isset($cellsArray[$y][$x])
+    ) {
+        openArea($x, $y);
+    }
+}
+
+function isBomb($x, $y)
+{
+    global $cellsArray, $lostGame;
+    if ($cellsArray[$y][$x]['isbomb'] == true 
+        && $cellsArray[$y][$x]['marked'] == false
+    ) {
+        $lostGame = true;
+    }
+    return $lostGame;
+}
+
+function openArea($x, $y)
+{
+    global $openedCellsCount, $cellsArray;
+    if ($cellsArray[$y][$x]['opened'] == false
+        && $cellsArray[$y][$x]['marked'] == false
+    ) {
+        $cellsArray[$y][$x]['opened'] = true;
+        $openedCellsCount += 1;
+        if ($cellsArray[$y][$x]['nearbycount'] != 0) {
+            return;
+        }
+    } else {
+        return;
+    }
+    for ($i = $x - 1; $i <= $x + 1; $i++) {
+        for ($j = $y - 1; $j <= $y + 1; $j++) {
+            openSurroundedCells($i, $j);
+        }
+    }
+}
+
+function setFlag($x, $y)
+{
+    global $openedCellsCount, $cellsArray;
+    if ($cellsArray[$y][$x]['marked'] == false) {
+        if ($cellsArray[$y][$x]['opened'] == false) {
+            $cellsArray[$y][$x]['marked'] = true;
+            $cellsArray[$y][$x]['opened'] = true;
+            $openedCellsCount++;
+        }
+    } else {
+            $cellsArray[$y][$x]['marked'] = false;
+            $cellsArray[$y][$x]['opened'] = false;
+            $openedCellsCount--;
+    }
+}
+?>
---
+++ src/Vars.php
@@ -0,0 +1,11 @@
+<?php
+
+define("MAX_X", 10);
+define("MAX_Y", 10);
+define("BOMBS_COUNT", 10);
+
+$cellsArray = array();
+$bombsArray = array();
+$openedCellsCount = 0;
+$lostGame = false;
+?>
